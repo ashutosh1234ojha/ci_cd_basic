9624d1cc7fab0f68e767815b79a39193674b168e
Ashutosh ojha <Ashutosh.Ojha2@commscope.com>
2025-10-08 22:58:04 +1100

    test case added

---
+++ app/src/main/java/com/example/ci_cd_basic/ui/Utility.kt
@@ -0,0 +1,8 @@
+package com.example.ci_cd_basic.ui
+
+class Utility {
+
+    fun addNumbers(a:Int,b:Int){
+        val sum = a+b
+    }
+}
---
+++ app/src/test/java/com/example/ci_cd_basic/ui/UtilityTest.kt
@@ -0,0 +1,17 @@
+package com.example.ci_cd_basic.ui
+
+import junit.framework.TestCase.assertEquals
+
+import org.junit.Test
+
+
+class UtilityTest {
+
+
+   @Test
+    fun addNumbers() {
+        assertEquals(4, 2 + 2)
+    }
+
+
+}
